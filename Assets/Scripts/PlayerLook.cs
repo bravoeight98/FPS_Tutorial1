769d38c6d9ca805e3a0bfa8fa75199bee0e4be58
Azfar Shams <azfarshams1998@gmail.com>
2023-08-18 16:29:21 +0600

    Update PlayerLook.cs

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -19,7 +19,7 @@ public class PlayerLook : MonoBehaviour
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         //apply this to our camera transform
-        cam.transform.localRotation = Quternion.Euler(xRotation, 0, 0);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         //rotate player to look left and right
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
     }
