1a1b47b13c97dcbc190c0423145903ae26f3302a
Azfar Shams <azfarshams1998@gmail.com>
2023-08-18 19:15:35 +0600

    Update Interactables.cs

--- Assets/Scripts/Interactables.cs
+++ Assets/Scripts/Interactables.cs
@@ -7,4 +7,12 @@ public abstract class Interactables : MonoBehaviour
     //messege displayed to player when looking ant an interactable object.
     public string promptMessage;
 
+    public void BaseInteract()
+    {
+        Interact();
+    }
+    protected virtual void Interact()
+    {
+        //template fuction overriden by subclass
+    }
 }
