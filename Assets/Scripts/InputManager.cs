2c3decd7adaad0a94a5f3df738b22d65af012d07
Azfar Shams <azfarshams1998@gmail.com>
2023-08-18 16:24:46 +0600

    Update InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -9,13 +9,17 @@ public class InputManager : MonoBehaviour
     private PlayerInput.OnFootActions onFoot;
 
     private PlayerMotor motor;
+    private PlayerLook look;
 
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+
         motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        
         onFoot.Jump.performed += ctx => motor.Jump();
     }
 
