df05d5daa62b9c3a350249a2a3a0bcbee892cb6b
Azfar Shams <azfarshams1998@gmail.com>
2023-08-18 16:06:26 +0600

    Update PlayerLook.cs

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    public Camera cam;
+    private float xRotation = 0f;
 
+    public float xSensitivity = 30f;
+    public float ySensitivity = 30f;
 }
