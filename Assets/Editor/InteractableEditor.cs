07d6b199352a719fe22315567cf01fec7c5c6194
Azfar Shams <azfarshams1998@gmail.com>
2023-08-19 10:55:08 +0600

    Update InteractableEditor.cs

--- Assets/Editor/InteractableEditor.cs
+++ Assets/Editor/InteractableEditor.cs
@@ -7,16 +7,24 @@ public class InteractableEditor : Editor
     public override void OnInspectorGUI()
     {
         Interactables interactable = (Interactables)target;
-        base.OnInspectorGUI();
-        if (interactable.useEvents)
+        if(target.GetType() == typeof(EventOnlyInteractable))
         {
-            if (interactable.GetComponent<InteractionEvent>() == null)
-                interactable.gameObject.AddComponent<InteractionEvent>();
+
         }
         else
         {
+
+            base.OnInspectorGUI();
+            if (interactable.useEvents)
+            {
+            if (interactable.GetComponent<InteractionEvent>() == null)
+                interactable.gameObject.AddComponent<InteractionEvent>();
+            }
+            else
+            {
             if (interactable.GetComponent<InteractionEvent>() != null)
                 DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+            }
         }
     }
 }
