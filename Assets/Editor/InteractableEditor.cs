5630cf1ddc5cc0af336ec4d2302082ce488c1225
Azfar Shams <azfarshams1998@gmail.com>
2023-08-19 10:25:59 +0600

    Update Open.anim

--- Assets/Editor/InteractableEditor.cs
+++ Assets/Editor/InteractableEditor.cs
@@ -6,6 +6,17 @@ public class InteractableEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        Interactables interactable = (Interactables)target;
         base.OnInspectorGUI();
+        if (interactable.useEvents)
+        {
+            if (interactable.GetComponent<InteractionEvent>() == null)
+                interactable.gameObject.AddComponent<InteractionEvent>();
+        }
+        else
+        {
+            if (interactable.GetComponent<InteractionEvent>() != null)
+                DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+        }
     }
 }
